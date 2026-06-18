e0a76924ffb83f3481831e97f4ff0387424f797f
inogami <dhetterjan23@gmail.com>
2026-06-18 10:21:47 +0800

    filling the Home page.

--- lib/features/home/presentation/home_page.dart
+++ lib/features/home/presentation/home_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pos_system/core/utilities/dimension.dart';
 import 'package:pos_system/core/widgets/text_formatter.dart';
 import 'package:pos_system/features/account/presentation/state_management/all_users_controller.dart';
+import 'package:pos_system/features/account/presentation/widget/logged_in_user_account_store_details.dart';
 
 class HomePage extends ConsumerStatefulWidget {
   const HomePage({super.key});
@@ -14,6 +16,8 @@ class _HomePageState extends ConsumerState<HomePage> {
   @override
   Widget build(BuildContext context) {
     final myColorScheme = Theme.of(context).colorScheme;
+    double width = MyDimensions.getWidth(context);
+    double height = MyDimensions.getHeight(context);
 
     // return Scaffold(
     //   backgroundColor: myColorScheme.surface,
@@ -21,7 +25,45 @@ class _HomePageState extends ConsumerState<HomePage> {
     // );
 
     return Scaffold(
-      body: Center(child: MyText(text: "Home Page")),
+      body: SafeArea(
+        child: Container(
+          width: width,
+          height: height,
+          padding: EdgeInsets.only(top: 10),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  SizedBox(width: 10),
+                  MyText(text: "Hello, ", fontSize: kDefaultFontSize + 4),
+                  MyText(
+                    text: "Name! ",
+                    fontSize: kDefaultFontSize + 7,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  MyText(text: "Welcome back!", fontSize: kDefaultFontSize + 4),
+                ],
+              ),
+              SizedBox(height: 20),
+
+              LoggedInUserAccountStoreDetails(),
+              SizedBox(height: 20),
+
+              Column(
+                children: [
+                  MyText(text: "Most Sold Product/Item"),
+                  MyText(text: "This week's trend"),
+                  MyText(text: "Out of Stock Product/Item"),
+                  MyText(text: "Staff icons"),
+                  MyText(text: "Day, week, month revenue"),
+                  MyText(text: "Day, week, month profit"),
+                  MyText(text: "Products near expiration date"),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
