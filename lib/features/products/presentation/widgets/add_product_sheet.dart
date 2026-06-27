abf5097b83038ed0232414127ae82c579127496c
inogami <dhetterjan23@gmail.com>
2026-06-27 23:22:46 +0800

    added a dae_picker in the add_product_sheet, and it is currently working well.

--- lib/features/products/presentation/widgets/add_product_sheet.dart
+++ lib/features/products/presentation/widgets/add_product_sheet.dart
@@ -1,8 +1,13 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hugeicons/hugeicons.dart';
+import 'package:pos_system/core/utilities/date_formatter.dart';
 import 'package:pos_system/core/widgets/bottom_sheet_decorated.dart';
+import 'package:pos_system/core/widgets/button.dart';
 import 'package:pos_system/core/widgets/container.dart';
+import 'package:pos_system/core/widgets/date_picker.dart';
 import 'package:pos_system/core/widgets/text_field.dart';
 import 'package:pos_system/core/widgets/text_formatter.dart';
 
@@ -14,59 +19,126 @@ class AddProductSheet extends ConsumerStatefulWidget {
 }
 
 class _AddProductSheetState extends ConsumerState<AddProductSheet> {
+  // Object Fields
+  TextEditingController productNameController = TextEditingController();
+  TextEditingController productDescriptionController = TextEditingController();
+  TextEditingController productRriceController = TextEditingController();
+  DateTime? expirationDate;
+
+  // Layout Fields
+  late ColorScheme myColorScheme;
+  late double width;
+  late double height;
+
+  @override
+  void dispose() {
+    productNameController.dispose();
+    productDescriptionController.dispose();
+    productRriceController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
-    final myColorScheme = Theme.of(context).colorScheme;
+    width = MediaQuery.of(context).size.width;
+    height = MediaQuery.of(context).size.height;
+    myColorScheme = Theme.of(context).colorScheme;
 
-    return MyDecoratedBottomSheet(
-      width: width,
-      // height: height * 0.56,
-      child: Column(
-        spacing: 8,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 8.0, bottom: 16),
-            child: MyText(
-              text: "Add a Product",
-              fontSize: kDefaultFontSize + 8,
-              fontWeight: FontWeight.w800,
+    return StatefulBuilder(
+      builder: (context, StateSetter setModalState) {
+        return MyDecoratedBottomSheet(
+          width: width,
+          // height: height * 0.56,
+          child: Column(
+            spacing: 8,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0, bottom: 16),
+                child: MyText(
+                  text: "Add a Product",
+                  fontSize: kDefaultFontSize + 8,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
+
+              MyTextfield(
+                labelText: "Product Name",
+                prefixIcon: HugeIcon(icon: HugeIcons.strokeRoundedPackage),
+                textController: productNameController,
+              ),
+              MyTextfield(
+                labelText: "Product Description",
+                prefixIcon: HugeIcon(icon: HugeIcons.strokeRoundedText),
+                textController: productDescriptionController,
+              ),
+              MyTextfield(
+                labelText: "Price",
+                prefixIcon: HugeIcon(icon: HugeIcons.strokeRoundedMoney04),
+                textController: productRriceController,
+              ),
+              MyTextfield(
+                labelText: "Get Barcode (Button ni dapat)",
+                prefixIcon: HugeIcon(icon: HugeIcons.strokeRoundedText),
+                textController: TextEditingController(),
+              ),
+              MyTextfield(
+                labelText: "Picture (Button ni dapat)",
+                prefixIcon: HugeIcon(icon: HugeIcons.strokeRoundedText),
+                textController: TextEditingController(),
+              ),
+              expirationDateButton(context, setModalState),
+              // MyTextfield(
+              //   labelText: "Expiration Date",
+              //   prefixIcon: HugeIcon(icon: HugeIcons.strokeRoundedCalendar05),
+              //   textController: TextEditingController(),
+              // ),
+              SizedBox(height: 8),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget expirationDateButton(BuildContext context, StateSetter setModalState) {
+    return GestureDetector(
+      onTap: () async {
+        expirationDate = await myDatePicker(context);
+        log(expirationDate.toString());
+        setModalState(() {});
+      },
+      child: MyContainer(
+        width: width * 0.8,
+        height: 50,
+        padding: EdgeInsets.only(left: 16),
+        borderRadius: 50,
+        color: myColorScheme.surfaceContainerHighest,
+        borderColor: myColorScheme.primaryFixed,
+        child:
+            // Column(
+            //   // mainAxisAlignment: MainAxisAlignment.start,
+            //   crossAxisAlignment: CrossAxisAlignment.start,
+            //   children: [
+            //     MyText(text: "Expiration Date"),
+            Row(
+              children: [
+                HugeIcon(
+                  icon: HugeIcons.strokeRoundedCalendar05,
+                  size: 32,
+                  color: myColorScheme.onSurfaceVariant,
+                ),
+                SizedBox(width: 8),
+                MyText(
+                  text: (expirationDate != null)
+                      ? MyDateFormatter.formatDate(
+                          dateTimeInString: expirationDate.toString(),
+                        )
+                      : "Pick Exp Date",
+                ),
+              ],
             ),
-          ),
-
-          MyTextfield(
-            labelText: "Product Name",
-            prefixIcon: HugeIcon(icon: HugeIcons.strokeRoundedPackage),
-            textController: TextEditingController(),
-          ),
-          MyTextfield(
-            labelText: "Product Description",
-            prefixIcon: HugeIcon(icon: HugeIcons.strokeRoundedText),
-            textController: TextEditingController(),
-          ),
-          MyTextfield(
-            labelText: "Price",
-            prefixIcon: HugeIcon(icon: HugeIcons.strokeRoundedMoney04),
-            textController: TextEditingController(),
-          ),
-          MyTextfield(
-            labelText: "Get Barcode (Button ni dapat)",
-            prefixIcon: HugeIcon(icon: HugeIcons.strokeRoundedText),
-            textController: TextEditingController(),
-          ),
-          MyTextfield(
-            labelText: "Picture (Button ni dapat)",
-            prefixIcon: HugeIcon(icon: HugeIcons.strokeRoundedText),
-            textController: TextEditingController(),
-          ),
-          MyTextfield(
-            labelText: "Expiration Date",
-            prefixIcon: HugeIcon(icon: HugeIcons.strokeRoundedCalendar05),
-            textController: TextEditingController(),
-          ),
-          SizedBox(height: 8),
-        ],
+        //   ],
+        // ),
       ),
     );
   }
