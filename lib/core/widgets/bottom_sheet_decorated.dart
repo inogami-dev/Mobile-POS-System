0625908068933016775bbf8b9fc20f886c02b7b3
inogami <dhetterjan23@gmail.com>
2026-08-04 20:41:29 +0800

    MyDecoratedBottomSheet's blur effect can now be adjusted or just disable it.

--- lib/core/widgets/bottom_sheet_decorated.dart
+++ lib/core/widgets/bottom_sheet_decorated.dart
@@ -7,12 +7,18 @@ class MyDecoratedBottomSheet extends StatelessWidget {
   final Widget child;
   final double? width;
   final double? height;
+  final bool enableBlurEffect;
+  final double blurSigmaX;
+  final double blurSigmaY;
 
   const MyDecoratedBottomSheet({
     super.key,
     required this.child,
     this.width,
     this.height,
+    this.enableBlurEffect = true,
+    this.blurSigmaX = 1.0,
+    this.blurSigmaY = 1.0,
   });
 
   @override
@@ -20,7 +26,10 @@ class MyDecoratedBottomSheet extends StatelessWidget {
     final myColorScheme = Theme.of(context).colorScheme;
 
     return BackdropFilter(
-      filter: ImageFilter.blur(sigmaX: 1, sigmaY: 1),
+      filter: ImageFilter.blur(
+        sigmaX: (enableBlurEffect) ? blurSigmaX : 0,
+        sigmaY: (enableBlurEffect) ? blurSigmaY : 0,
+      ),
       child: MyContainer(
         width: width,
         height: height,
